9f49f960e54317b9349739cc696bba7980a3084a
DipaWidia <wiartha2001@gmail.com>
2022-04-21 10:43:23 +0800

    add api route for menus & categories, add api controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+  root 'pages#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  
+  namespace :api do
+    namespace :v1 do
+      resources :menus
+      resources :categories
+    end
+  end
+
+  get '*path', to: 'pages#index', via: :all
 end
