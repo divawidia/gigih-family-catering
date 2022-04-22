37c35f43b19c66fac3af3dc4ca176ca35436f812
DipaWidia <wiartha2001@gmail.com>
2022-04-22 22:27:19 +0800

    add menu and order_details validation

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,4 +1,8 @@
 class Menu < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :price, presence: true, comparison: { greater_than: 0.01 }, numericality: true
+    validates :description, presence: true, length: { maximum: 150 }
+    
     has_many :menus_categories
     has_many :categories, through: :menus_categories
 
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,9 @@
 class OrderDetail < ApplicationRecord
+  validates :order_id, presence: true
+  validates :menu_id, presence: true
+  validates :subtotal, presence: true
+  validates :qty, presence: true
+
   belongs_to :order
   belongs_to :menu
 end
